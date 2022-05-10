a909b72c5b7c7faf55cee96793d8ed7aff5d0c2d
Shaun Duncan <shaun@speedscale.com>
2022-05-10 17:47:11 -0400

    change variable scoping

--- Formula/speedctl.rb
+++ Formula/speedctl.rb
@@ -6,31 +6,31 @@ class Speedctl < Formula
   version "0.12.117"
 
   # which binary do we need
-  @os = ""
-  @arch = ""
+  @@os = ""
+  @@arch = ""
 
-  on_macos { @os = "darwin" }
-  on_linux { @os = "linux" }
+  on_macos { @@os = "darwin" }
+  on_linux { @@os = "linux" }
 
   if Hardware::CPU.is_64_bit?
     if Hardware::CPU.intel?
-      @arch = "amd64"
+      @@arch = "amd64"
     end
 
     if Hardware::CPU.arm?
-      @arch = "arm64"
+      @@arch = "arm64"
     end
   end
 
-  if @os != "" and @arch != ""
-    url "https://downloads.speedscale.com/speedctl/v#{version}/speedctl-#{@os}-#{@arch}"
+  if @@os != "" and @@arch != ""
+    url "https://downloads.speedscale.com/speedctl/v#{version}/speedctl-#{@@os}-#{@@arch}"
 
     # grab the checksum file
     # URI.open("#{url}.sha256") { |f| sha256 f.body.strip }
   end
 
   def install
-    bin.install "speedctl-#{@os}-#{@arch}" => "speedctl"
+    bin.install "speedctl-#{@@os}-#{@@arch}" => "speedctl"
 
     # generate the completions
     (bash_completion/"speedctl").write Utils.safe_popen_read(bin/"speedctl", "completion", "bash")
