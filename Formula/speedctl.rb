0198cc7d4dced763b9540ad774fb3145f3786400
speedscale-ci <ci@speedscale.com>
2023-12-04 20:05:26 +0000

    Bump to 1.4.120

--- Formula/speedctl.rb
+++ Formula/speedctl.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 class Speedctl < Formula
   desc "Speedscale CLI"
   homepage "https://speedscale.com/"
-  version "1.4.100"
+  version "1.4.120"
 
   # which binary do we need
   @@os = ""
