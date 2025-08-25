3899039dcca4a58ac88ca8bff2716b99c008fbb1
speedscale-ci <ci@speedscale.com>
2025-08-25 15:23:39 +0000

    Bump to 2.3.747

--- Formula/proxymock.rb
+++ Formula/proxymock.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 class Proxymock < Formula
   desc "proxymock CLI"
   homepage "https://speedscale.com/"
-  version "2.3.738"
+  version "2.3.747"
 
   # which binary do we need
   @@os = ""
